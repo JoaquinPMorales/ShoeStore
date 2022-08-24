7828da7cec74a6c98e191fd2821226758ad62ed5
JoaquinPMorales <joaquin.perez@partner.bmw.de>
2022-08-24 13:24:22 +0200

    added go back logic and logout menu

--- app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
@@ -3,15 +3,16 @@ package com.udacity.shoestore
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
 import java.util.*
 
 class ShoeViewModel : ViewModel() {
-    private val _listOfShoes = MutableLiveData<Vector<String>>()
+    private val _listOfShoes = MutableLiveData<Vector<Shoe>>()
 
-    val listOfShoes : LiveData<Vector<String>>
+    val listOfShoes : LiveData<Vector<Shoe>>
         get() = _listOfShoes
 
-    fun addNewShoe(newShoe : String)
+    fun addNewShoe(newShoe : Shoe)
     {
         _listOfShoes.value?.add(newShoe)
     }
--- app/src/main/java/com/udacity/shoestore/screens/list/ListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/list/ListFragment.kt
@@ -1,9 +1,7 @@
 package com.udacity.shoestore.screens.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -21,9 +19,27 @@ class ListFragment : Fragment() {
         val binding: ListFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.list_fragment, container, false)
 
+        setHasOptionsMenu(true)
+
         binding.listButton.setOnClickListener {
             findNavController().navigate(ListFragmentDirections.actionListFragmentToDetailFragment())
         }
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        val inflater: MenuInflater = inflater
+        inflater.inflate(R.menu.logout_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.logout_item -> {
+                findNavController().navigate(ListFragmentDirections.actionListFragmentToLoginFragment())
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
